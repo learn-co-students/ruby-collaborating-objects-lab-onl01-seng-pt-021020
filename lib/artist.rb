3d9c9cca6da5490c5977ea74549f8f8ba30c78f9
Amber Williams <awilliams1234@live.com>
2020-03-08 18:46:14 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,24 +2,37 @@ class Artist
   attr_accessor :name 
   attr_reader :songs  
   
-  @@all = []
-  
   def initialize(name)
     @name = name
     @songs = []
   end 
   
+  @@all = []
+  
   def self.all
     @@all 
   end
   
-  def add_song 
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create_by_name(name)
+  end 
+  
+  def self.find_by_name(name)
+    @@all.detect do |artist|
+      artist.name == name 
+    end 
+  end 
+  
+  def self.create_by_name(name)
+    self.name(name).save
+  end 
+  
+  def add_song(song) 
     @songs.push(song)
   end 
   
-  def save
-    @@all.push(self) 
+  def save 
+    @@all.push(self)
+    self 
   end 
-    
-  
 end 
