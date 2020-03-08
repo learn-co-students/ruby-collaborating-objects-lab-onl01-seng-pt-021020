6f113158af3d25aeadf8058ec7df62f74b62ea9f
Amber Williams <awilliams1234@live.com>
2020-03-08 18:17:34 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,8 +1,22 @@
 class Artist 
+  attr_accessor :name 
+  attr_reader :song 
+  
   @@all = []
   
   def initialize(name)
-    @name = name 
+    @name = name
+    @song = []
   end 
+  
+  def self.all
+    @@all 
+  end
+  
+  def add_song 
+  end 
+  
+  def save
+    
   
 end 
