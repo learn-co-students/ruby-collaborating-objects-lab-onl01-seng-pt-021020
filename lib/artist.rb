5b459b43e2dee9d17026c4f20db87ffaab2d9308
Cameron J Leverett <cameron.leverett@yahoo.com>
2020-03-09 19:02:12 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -10,20 +10,37 @@ def initialize(name)
 end
 
 def add_song(song)
-@songs << song
-self.song
+  song.artist = self
+  @songs << song
+    self.song
+
 end
 
 def songs
   @songs
+
 end
 
   def name
     @name
   end
 
+
+def self.find_or_create_by_name(name)
+  found = self.all.find {|artist| artist.name == name}
+if found
+  found
+else new_artist = self.new(name)
+new_artist
+end
+end
+
 def self.all
 @@all
+end
+def print_songs
+  songs.each {|s| puts s.name}
+  songs
 end
 
 end
--- lib/song.rb
+++ lib/song.rb
@@ -11,8 +11,22 @@ def self.all
 
 end
 
+def self.new_by_filename(file_name)
+artist_name, song_name, genre_name = file_name.chomp(".mp3").split(" - ")
+song = Song.new(song_name)
+artist = Artist.new(artist_name)
+artist.add_song(song)
+song
+
+
+end
+
+
 def artist_name=(name)
   @name = name
+ self.artist = Artist.find_or_create_by_name(name)
+ artist.add_song(self)
+
 end
 
 
